9a4b5dc1fa4022dee9ec42759a98aceef9b13c6a
Chris Lucani <chrislucani@yahoo.co.uk>
2016-12-30 23:38:19 +1100

    blah

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -1,10 +1,16 @@
 // public/js/controllers/MainCtrl.js
 angular.module('MainCtrl', []).controller('MainController', function($scope, socket) {
 
-  $scope.startLED = function() {
-    console.log("Starting led");
+  $state.LEDState = false;
+
+  socket.on('LEDStateChange', function(newState) {
+    console.log('LEDStateChange', newState);
+    $state.LEDState = newState;
+  })
+
+  $scope.updateLED = function() {
+    console.log("Updating led");
     socket.emit('led');
   }
-
 
 });
--- server.js
+++ server.js
@@ -44,18 +44,8 @@ io.on('connection', function(socket){
     var state = led.readSync();
 
     led.writeSync(Number(!state));
+    socket.emit('LEDStateChange', !state);
 
-    //
-    // var iv = setInterval(function(){
-    //   led.writeSync(led.readSync() === 0 ? 1 : 0)
-    // }, 500);
-    //
-    // // Stop blinking the LED and turn it off after 5 seconds.
-    // setTimeout(function() {
-    //   clearInterval(iv); // Stop blinking
-    //   led.writeSync(0);  // Turn LED off.
-    //   led.unexport();    // Unexport GPIO and free resources
-    // }, 5000);
   });
 });
 
