f0dff7131a8e49be931496ca8d70b98716fe4859
Chris Lucani <chrislucani@yahoo.co.uk>
2016-12-30 22:01:22 +1100

    init

---
+++ app/routes.js
@@ -0,0 +1,30 @@
+// app/routes.js
+
+module.exports = function(app, Gpio) {
+
+  var led = new Gpio(17, 'out');
+
+  // sample api route
+  app.get('/led', function(req, res) {
+
+    var iv = setInterval(function(){
+      led.writeSync(led.readSync() === 0 ? 1 : 0)
+    }, 500);
+
+    // Stop blinking the LED and turn it off after 5 seconds.
+    setTimeout(function() {
+      clearInterval(iv); // Stop blinking
+      led.writeSync(0);  // Turn LED off.
+      led.unexport();    // Unexport GPIO and free resources
+    }, 5000);
+
+    res.send('fabulous');
+  });
+
+  // frontend routes =========================================================
+  // route to handle all angular requests
+  app.get('*', function(req, res) {
+      res.sendfile('./public/views/index.html'); // load our public/index.html file
+  });
+
+};
---
+++ public/js/appRoutes.js
@@ -0,0 +1,13 @@
+// public/js/appRoutes.js
+angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+  $routeProvider
+
+    .when('/', {
+      templateUrl: 'views/home.html',
+      controller: 'MainController'
+    })
+
+  $locationProvider.html5Mode(true);
+
+}]);
