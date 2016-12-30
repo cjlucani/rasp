344721b6175e9b8120a26de184e80b34e7585af7
Chris Lucani <chrislucani@yahoo.co.uk>
2016-12-30 23:01:12 +1100

    blah

--- app/routes.js
+++ app/routes.js
@@ -23,7 +23,7 @@ module.exports = function(app, Gpio) {
 
   // frontend routes =========================================================
   // route to handle all angular requests
-  app.get('*', function(req, res) {
+  app.get('/', function(req, res) {
       res.sendfile('./public/views/index.html'); // load our public/index.html file
   });
 
