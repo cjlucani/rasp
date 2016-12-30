1b64ee0462fefa38e2b3a8a787c25a7cca6e66a2
Chris Lucani <chrislucani@yahoo.co.uk>
2016-12-30 23:29:38 +1100

    blah

--- server.js
+++ server.js
@@ -41,16 +41,21 @@ io.on('connection', function(socket){
   socket.on('led', function() {
     console.log('led emit');
 
-    var iv = setInterval(function(){
-      led.writeSync(led.readSync() === 0 ? 1 : 0)
-    }, 500);
+    var state = led.readSync();
 
-    // Stop blinking the LED and turn it off after 5 seconds.
-    setTimeout(function() {
-      clearInterval(iv); // Stop blinking
-      led.writeSync(0);  // Turn LED off.
-      led.unexport();    // Unexport GPIO and free resources
-    }, 5000);
+    led.writeSync(Number(!state));
+
+    //
+    // var iv = setInterval(function(){
+    //   led.writeSync(led.readSync() === 0 ? 1 : 0)
+    // }, 500);
+    //
+    // // Stop blinking the LED and turn it off after 5 seconds.
+    // setTimeout(function() {
+    //   clearInterval(iv); // Stop blinking
+    //   led.writeSync(0);  // Turn LED off.
+    //   led.unexport();    // Unexport GPIO and free resources
+    // }, 5000);
   });
 });
 
